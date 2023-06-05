18d424b15407665ec205315bd8349d737b45d96d
Vasu Kaushik <kaushikvasu98@gmail.com>
2023-06-05 11:38:23 +0530

    added models

---
+++ eTickets/Models/Producer.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace eTickets.Models
+{
+    public class Producer
+    {
+        [Key]
+        public int ProducerId { get; set; }
+        public string ProfilePictureURL { get; set; }
+        public string FullName { get; set; }
+        public string Bio { get; set; }
+
+        //Relationships
+        public List<Movie> Movies { get; set; }
+
+        
+
+    }
+}
